9e5c6528a8c48da4992a2c68669ac04063b1675f
micaza <icazamartinez@gmail.com>
2018-01-18 00:20:16 -0300

    Added the progress bar functionality

--- youtube/main.py
+++ youtube/main.py
@@ -4,6 +4,7 @@ import youtube_dl
 # Required Tkinter module for GUI
 try:
     import tkinter as tk
+    from tkinter import ttk
 except ImportError:
     raise SystemExit("The Tkinter module is required for the graphical interface")
 
@@ -12,6 +13,18 @@ window = tk.Tk()
 
 # Variable for storage URL
 youtube_url = tk.StringVar()
+
+# Progress bar widget
+progress_bar = ttk.Progressbar(window, orient="horizontal", length=200, mode="determinate")
+
+
+# Hook for progress of the process
+def hook(progress):
+    global progress_bar
+    print(progress['status'])
+    if progress['status'] == 'downloading':
+        progress_bar["value"] = progress['_percent_str'].replace('%', '')
+        progress_bar.update_idletasks()
 
 
 # Convert video to mp3 format
@@ -24,6 +37,7 @@ def convert():
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
+        'progress_hooks': [hook]
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -46,5 +60,6 @@ def run():
     label.grid(row=0, column=1, padx=5)
     entry.grid(row=0, column=2, padx=5)
     button.grid(row=0, column=3, pady=10)
+    progress_bar.grid(row=1, rowspan=2, columnspan=5)
 
     window.mainloop()
